f7c3d2b3cbb9c309393346ff267f8efa1f941846
rublev <r@rublev.io>
2018-08-27 18:55:36 -0400

    add wall component

---
+++ src/components/Wall/Wall.vue
@@ -0,0 +1,18 @@
+<style lang='scss' scoped src='./style.scss' />
+
+<script>
+
+	export default {
+		name: 'wall',
+		data: () => ({
+			blockstack: window.blockstack,
+		}),
+	}
+
+</script>
+
+<template>
+	<div class='wall'>
+		wall
+	</div>
+</template>
