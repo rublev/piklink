7733b83de4dd2a92198a748389d6ac5fc99de994
rublev <r@rublev.io>
2018-08-27 15:04:01 -0400

    add libraries to main.js and file missing from previous commit (config/index.js)

--- src/main.js
+++ src/main.js
@@ -1,17 +1,35 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
+import { sync } from 'vuex-router-sync'
+import _ from 'lodash'
+import * as svgicon from 'vue-svgicon'
+
 import App from '@/containers/App/App'
 import router from '@/router'
 
+import store from './store'
+
+Vue.use(Vuex)
+
+sync(store, router)
+
 window.blockstack = require('blockstack')
+
+Vue.set(Vue.prototype, '_', _)
+
+Vue.use(svgicon, {
+	tagName: 'svgicon'
+})
 
 Vue.config.productionTip = false
 
+Vue.config.ignoredElements = ['ion-icon']
+
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
+	el: '#app',
+	store,
+	router,
   components: { App },
   template: '<App/>'
 })
