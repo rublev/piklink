96b17a884d825eae322d15916838da76f3f6b547
rublev <r@rublev.io>
2018-08-27 18:54:35 -0400

    super basic image upload

--- src/components/Upload/Upload.vue
+++ src/components/Upload/Upload.vue
@@ -2,17 +2,48 @@
 
 <script>
 
+	import md5 from 'md5'
+
 	export default {
 		name: 'upload',
 		data: () => ({
 			blockstack: window.blockstack,
+			file: null
 		}),
+		methods: {
+			onFileChanged (event) {
+				this.file = event.target.files[0]
+			},
+			upload () {
+				this.readFile(this.file)
+			},
+			readFile (file) {
+				const filereader = new FileReader()
+				filereader.onload = event => {
+					const result = event.target.result
+					const path = 'images/' + md5(result)
+					this.uploadImageAndUpdateIndex(path, result)
+				}
+				console.log('file', file)
+				filereader.readAsDataURL(file)
+			},
+			uploadImageAndUpdateIndex(path, result) {
+				blockstack.putFile(path, result)
+					.then(fileUrl => {
+						console.log(fileUrl);
+					})
+					.catch((e) => {
+						console.error(e)
+					})
+			}
+		}
 	}
 
 </script>
 
 <template>
 	<div class='upload'>
-		upload
+		<input type='file' @change='onFileChanged'>
+		<button @click='upload'>Upload!</button>
 	</div>
 </template>
