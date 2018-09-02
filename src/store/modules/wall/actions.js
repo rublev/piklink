bdd2e5dbed32a441747784184d4e9f40f4efff73
rublev <r@rublev.io>
2018-09-02 18:04:44 -0400

    remove dead actions

--- src/store/modules/wall/actions.js
+++ src/store/modules/wall/actions.js
@@ -1,12 +1,7 @@
-export const updateImages = ({ commit, state, rootState }, images) => {
-	commit('UPDATE_IMAGES', images)
-}
-
 export const updateAllImages = ({ commit }, imagesObj) => {
 	commit('UPDATE_ALL_IMAGES', imagesObj)
 }
 
 export default {
-	updateImages,
 	updateAllImages,
 }
