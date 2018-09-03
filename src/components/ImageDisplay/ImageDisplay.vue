cdbe2351c0a8d89b8dc7077337b3e455a6ab9702
rublev <r@rublev.io>
2018-09-03 09:23:36 -0400

    remove dead code

--- src/components/ImageDisplay/ImageDisplay.vue
+++ src/components/ImageDisplay/ImageDisplay.vue
@@ -2,7 +2,7 @@
 
 <script>
 
-	import { mapState, mapActions } from 'vuex'
+	import { mapState } from 'vuex'
 	import moment from 'moment'
 
 	export default {
@@ -12,32 +12,17 @@
 		}),
 		computed: {
 			...mapState({
-				'modalImage': state => state.wall.modalImage,
-				'id': state => state.wall.modalImage.id,
-				'date': state => state.wall.modalImage.date,
-				'avatar': state => state.wall.modalImage.avatar,
+				'data': state => state.wall.modalImage.data,
 				'created': state => moment(state.wall.modalImage.created).format('dddd, MMMM Do, YYYY'),
-				'images': state => state.wall.images,
 			}),
 		},
-		methods: {
-			...mapActions({
-				fetchImage: 'wall/fetchImage', // remove ??
-			}),
-		},
-		// watch: {
-		// 	modalImage
-		// },
-		mounted() {
-			console.log(moment('2010-10-20 4:30 +0000').format('dddd, MMMM Do, YYYY'))
-		}
 	}
 
 </script>
 
 <template>
 	<div class='image-display'>
-		<img :src='modalImage.data' class='modal-image'/>
+		<img :src='data' class='modal-image'/>
 		<div class='details'>
 			{{ created }}
 		</div>
