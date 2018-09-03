bf10e9bf2527a4cd70d816b92fa2009ed2b660f8
rublev <r@rublev.io>
2018-09-02 20:40:32 -0400

    add modalimage to wall state

--- src/store/modules/wall/actions.js
+++ src/store/modules/wall/actions.js
@@ -2,6 +2,14 @@ export const updateImages = ({ commit }, images) => {
 	commit('UPDATE_IMAGES', images)
 }
 
+export const fetchImage = ({ state, commit }, id) => {
+	const image = _.find(state.images, obj => {
+		return obj.id == id
+	})
+	commit('SELECT_IMAGE_FOR_MODAL', image.data)
+}
+
 export default {
 	updateImages,
+	fetchImage,
 }
--- src/store/modules/wall/mutations.js
+++ src/store/modules/wall/mutations.js
@@ -6,7 +6,12 @@ const RESET_IMAGES = state => {
 	state.images = []
 }
 
+const SELECT_IMAGE_FOR_MODAL = (state, image) => {
+	state.modalImage = image
+}
+
 export default {
 	UPDATE_IMAGES,
 	RESET_IMAGES,
+	SELECT_IMAGE_FOR_MODAL,
 }
