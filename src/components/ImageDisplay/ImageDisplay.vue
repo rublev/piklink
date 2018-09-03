beaa098d1cf15883f191c78934a2a4e3ead1faf3
rublev <r@rublev.io>
2018-09-02 20:39:32 -0400

    basic imagedisplay modal container component

---
+++ src/components/ImageDisplay/ImageDisplay.vue
@@ -0,0 +1,28 @@
+<style lang='scss' scoped src='./style.scss' />
+
+<script>
+
+	import { mapState, mapActions } from 'vuex'
+
+	export default {
+		name: 'image-display',
+		computed: {
+			...mapState({
+				'modalImage': state => state.wall.modalImage,
+				'images': state => state.wall.images,
+			}),
+		},
+		methods: {
+			...mapActions({
+				fetchImage: 'wall/fetchImage',
+			}),
+		},
+	}
+
+</script>
+
+<template>
+	<div class='image-display'>
+		<img :src='modalImage' />
+	</div>
+</template>
